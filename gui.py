e31c5796744007219d893f4b2b010428bcb94da3
Abdullah <xzombozz@gmail.com>
2024-07-25 21:21:45 +1000

    updated gui

--- gui.py
+++ gui.py
@@ -1,4 +1,8 @@
 import PySimpleGUI as sg
 
 def setup_gui():
-    sg.Window(title="Hello World", layout=[[]], margins=(800, 500)).read()
+    layout= [
+        [sg.Text('Twitter Username'), sg.Input(key='-USERNAME-')],
+        [sg.Button('Get User Details'), sg.Button('Exit')]
+    ]
+    sg.Window(title="Hello World", layout=layout, margins=(800, 500)).read()
